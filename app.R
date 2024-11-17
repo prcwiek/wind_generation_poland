b60b001422fd9399d38537950a9b817dab3922dc
Pawel Cwiek <prc.altodato@gmail.com>
2024-11-17 20:58:44 +0100

    change to bslib

--- app.R
+++ app.R
@@ -1,106 +1,189 @@
-library(shiny)
-library(tidyverse)
+library(bslib)
 library(lubridate)
+library(scales)
+library(shiny)
+library(shinyjs)
+library(tidyverse)
 
+# Version with data from the year 2016
 load("data/wind_generation_PL2016.RData")
 
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Wind energy generation in Poland 2016"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         dateInput("sdate", "Start date:", value = "2016-01-01", format = "yyyy-mm-dd",
-                   min = "2016-01-01", max = "2016-12-31"),
-         dateInput("edate", "End date:", value = "2016-12-31", format = "yyyy-mm-dd",
-                   min = "2016-01-01", max = "2016-12-31"),
-         br(),h4("Source of data:"),
-         p("Information, data, obtained from the site www.pse.pl, according to the status of the site on 18/06/2018, partially processed"),
-         p("Informacje, dane, pozyskane ze strony www.pse.pl wg. stanu strony na dzień 18.06.2018, przetworzona w części."),
-         a("www.pse.pl", href = "www.pse.pl"),
-         br(),br(),p("All files are availbel on my Github"),
-         a("www.github.com/prcwiek/wind_generation_poland", href = "www.github.com/prcwiek/wind_generation_poland")
-         
-      ),
+cards <- list(
+  card(
+    full_screen = TRUE,
+    card_header <- "Wind generation (MWh) in selected period",
+    plotOutput("genPlot")
+  ),
+  card(
+    full_screen = TRUE,
+    #height = "200px",
+    card_header("Monthly values for 2016"),
+    plotOutput("barPlot", height = "80%")
+  )
+)
 
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("genPlot"),
-         h4("Summary of wind energy generation (MWh) in the selected period"),
-         verbatimTextOutput("summary"),
-         br(),h4("Total generation in the selected period"),
-         verbatimTextOutput("total_generation"),
-         br(),h4("Monthly values for 2016"),
-         plotOutput("barPlot")
-      )
-   )
+ui <- page_sidebar(
+
+  shinyjs::useShinyjs(),
+  
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "cosmo",
+    base_font = font_google("Inter"),
+    navbar_bg = bs_get_variables(bs_theme(bootswatch = "cosmo"), "primary") 
+  ),
+  
+  title = "Wind energy generation in Poland 2016", 
+  
+  sidebar = sidebar(
+    title = "Data range",
+    dateInput("sdate", "Start date:", value = "2016-01-01", format = "yyyy-mm-dd",
+              min = "2016-01-01", max = "2016-12-31"),
+    dateInput("edate", "End date:", value = "2016-12-31", format = "yyyy-mm-dd",
+    min = "2016-01-01", max = "2016-12-31"),
+    br(),h5("Source of data:"),
+    p("Information, data, obtained from the site www.pse.pl, according to the status of the site on 18/06/2018, partially processed"),
+    p("Informacje, dane, pozyskane ze strony www.pse.pl wg. stanu strony na dzień 18.06.2018, przetworzona w części."),
+    a("www.pse.pl", href = "www.pse.pl"),
+   ),
+  
+  layout_columns(
+    value_box(
+      title = "Total generation in the selected period",
+      value = textOutput("generation_sum"),
+      showcase = bsicons::bs_icon("graph-up"),
+      fill = TRUE,
+      theme = "primary"
+    ),
+    value_box(
+      title = textOutput("generation_min_date"),
+      value = textOutput("generation_min"),
+      showcase = bsicons::bs_icon("graph-down-arrow"),
+      fill = TRUE,
+      theme = "danger"
+    ),
+    value_box(
+      title = textOutput("generation_max_date"),
+      value = textOutput("generation_max"),
+      showcase = bsicons::bs_icon("graph-up-arrow"),
+      fill = TRUE,
+      theme = "success"
+    )
+  ),
+  
+  layout_columns(
+    col_widths = c(12, 12),
+    row_heights = c(1,1),
+    cards[[1]],
+    cards[[2]]
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-   
+server <- function(input, output, session) {
+  
   df <- reactive({
     req(input$sdate, input$edate)
     edate <- make_datetime(year = year(input$edate),
                            month = month(input$edate),
                            day = day(input$edate),
                            hour = 23, min = 00, tz ="UTC")
-    dg16 %>% 
-      select(date, generation.MWh) %>% 
-      filter(date >= input$sdate & date <= edate) %>% 
-      group_by(date) %>% 
+    dg16 |> 
+      select(date, month, generation.MWh) |> 
+      filter(date >= input$sdate & date <= input$edate) |> 
+      group_by(date) |> 
       summarise(generation.MWh = sum(generation.MWh))
+    
   })
   
-   output$genPlot <- renderPlot({
-     ggplot(data = df(),aes(x = date, y = generation.MWh, color = generation.MWh)) +
-       xlab("Time") +
-       ylab("Wind energy generation (MWh)") +
-       geom_line() +
-       theme_minimal() +
-       theme(legend.position = "none")
-   })
-   
-   output$summary <- renderPrint({
-     summary(df()$generation.MWh)
-   })
-   
-   output$total_generation <- renderPrint({
-     req(input$sdate, input$edate)
-     edate <- make_datetime(year = year(input$edate),
-                            month = month(input$edate),
-                            day = day(input$edate),
-                            hour = 23, min = 00, tz ="UTC")
-     df_tg <- dg16 %>%
-       select(date, generation.MWh) %>%
-       filter(date >= input$sdate & date <= edate)
-     cat("  ")
-     cat(format(sum(df_tg$generation.MWh), big.mark = ","))
-     cat(" MWh")
-   })
-   
-   output$barPlot <- renderPlot({
-     dfp <- dg16 %>% 
-       select(month, generation.MWh) %>% 
-       group_by(month) %>% 
-       summarise(generation.MWh = sum(generation.MWh))
-     
-     dfp$month <- gl(12,1,12,
-                    labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
-     
-     ggplot(dfp) +
-       xlab("Month of 2016") +
-       ylab("Wind energy generation (MWh)") +
-       geom_col(aes(x = month, y = generation.MWh, fill = generation.MWh)) +
-       geom_text(aes(x = month, y = generation.MWh, 
-                     label = format(round(generation.MWh, 0), big.mark = ",")),
-                 size = 4, vjust = -0.5) +
-       theme_minimal() +
-       theme(legend.position = "none") 
-   })
+  output$genPlot <- renderPlot({
+    ggplot(data = df(), aes(x = date, y = generation.MWh, color = generation.MWh)) +
+      geom_line() +
+      scale_y_continuous(
+        breaks = seq(0, 7000, 1000),
+        labels = label_number(big.mark = ","),
+        limits = c(0, 7000)
+      ) + 
+      labs(
+        x = ("Time"),
+        y = ("Wind energy generation (MWh)"),
+        position = "left",
+      ) +
+      theme_minimal() +
+      theme(legend.position = "none")
+  })
+
+  output$barPlot <- renderPlot({
+    dfp <- dg16 |>
+      mutate(months_labels = as.character(month)) |> 
+      mutate(months_labels = factor(months_labels, 
+                                    labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                                               "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))) |> 
+      filter(date >= input$sdate & date <= input$edate) |> 
+      select(month, months_labels, generation.MWh) |>
+      group_by(months_labels) |>
+      summarise(generation.MWh = sum(generation.MWh)) 
+
+    
+    ggplot(dfp) +
+      geom_col(aes(x = months_labels, y = generation.MWh, fill = generation.MWh)) +
+      geom_text(aes(x = months_labels, y = generation.MWh, 
+                    label = format(round(generation.MWh, 0), big.mark = ",")),
+                size = 4, vjust = -0.5) +
+      scale_y_continuous(
+        breaks = c(0, 500000, 1000000, 1500000, 2000000),
+        labels = label_number(big.mark = ","),
+        position = "left",
+        limits = c(0, 1600000)) +
+      labs(
+        x = "Month of 2016",
+        y = "Wind energy generation (MWh)"
+      ) +
+      theme_minimal() +
+      theme(legend.position = "none") 
+
+      
+  })
+  
+
+  
+  output$generation_sum <- renderText({
+    paste0(
+      format(round(sum(df()$generation.MWh), 0), big.mark = ","),
+      " MWh"
+    )
+  })
+  
+  output$generation_min <- renderText({
+    paste0(
+      format(round(min(df()$generation.MWh), 0), big.mark = ","),
+      " MWh"
+    )
+  })
+
+  output$generation_min_date <- renderText({
+    paste0(
+      "Lowest generation on ",
+      as.Date(df()$date[which.min(df()$generation.MWh)])
+    )
+  })
+
+  output$generation_max <- renderText({
+    paste0(
+      format(round(max(df()$generation.MWh), 0), big.mark = ","),
+      " MWh"
+    )
+  })
+  output$generation_max_date <- renderText({
+    paste0(
+      "Highest generation on ",
+      as.Date(df()$date[which.min(df()$generation.MWh)])
+    )
+  })
+  
+  # End application when a window or a tab is closed
+  session$onSessionEnded(stopApp)
+  
 }
 
 # Run the application 
